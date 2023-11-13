3a70397a332ed4aaf9055be77dcec70ebbb8a824
bludol <61668069+bludol@users.noreply.github.com>
2023-11-13 16:11:02 +0100

    comment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def load_json_elements(json_file):
     with open(json_file, 'r', encoding='utf-8') as file:
         groups = json.load(file)
     return groups
-
+#nacteni dat ze souboru groups.json
 
 def load_csv_elements(csv_file):
     elements = []
@@ -21,44 +21,48 @@ def load_csv_elements(csv_file):
             elements.append(row)
             csv_row = ",".join(row)
     return elements
+#nacteni dat ze souboru elements.csv
 
 
 def vyber():
     print("1: Vyhledavani prvku")
     print("2: Periodicka tabulka")
-    print("3: Vypocet")
-    print("4: Vypis skupiny prvku")
-    print("5: Vypnout program")
-
+    print("3: Vypis skupiny prvku")
+    print("4: Vypnout program")
+#funkce na vypsání vývběru
 
 def main():
     elements = load_csv_elements('elements.csv')
     groups = load_json_elements('groups.json')
-
+#vyvolani funkci k načtení dat
     while True:
         vyber()
-
+#vypis vyběru
         volba = input("Zadej svou volbu: ")
+#uživatelský vstup
         if volba == "1":
-            prvek = input("Zadej prvek, který chceš vyhledat: ")
+            prvek = input("Zadej protonové číslo, název nebo značku prvku, který chceš vyhledat: ")
             vyskyt = hledani.hledani(elements, prvek)
+#Pokud je vybrána volba "1". Vyžádáme si vsatup pro hledání který pošleme do fuknce hledani s parametry elements a prvek.
             if vyskyt:
                 hledani.vypis_samotneho_prvku(vyskyt, elements)
+#Pokud je prvek nalezen, vypíše se pomocí funkce vypis_samostatneho_prvku s parametry vyskyt, elements.
         elif volba == "2":
             tabulka.tabulka(elements)
             path = os.path.abspath("index.html")
             url = "file://" + path
             webbrowser.open(url)
+#Pokud je volba "2" Vytvoři se peridoická soustava prvků v tabulce a uloží se do souboru index.html.
         elif volba == "3":
-            print("neni dodelano")
-        elif volba == "4":
             VolbaSkupiny = input("zadej nazev skupiny: ")
             vyskyt = hledani.vypis_skupin(VolbaSkupiny, groups)
             if vyskyt:
                 print(vyskyt)
-        elif volba == "5":
+#Pokud je volba "3" Vyžádáme si vstup skupiny, kterou chceme hledat. vstup se pošle dofunkce vypis_skupin s parametry VolbaSkupiny, V případě nálezu se vypíše skupina.
+        elif volba == "4":
             remove("index.html")
             break
+#Při volbě "4" se vymaže index.html a ukončí se program.
 
 
 main()
